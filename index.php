e9fba9c8cf5cf2805021484e211b80b0629eff7b
kraquipiso <46570473+kraquipiso@users.noreply.github.com>
2019-01-10 08:20:53 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,17 +4,16 @@ include('inc/header.php');
 ?>
 
   <p id="store-info">Musique Plastique
-    1627 NE Alberta St. Suite 5
+    1627 NE Alberta St.  
     Portland, OR 97211
-    <i>located inside the Alberta Studios Building</i>
-
+ 
     Store hours:
     Everyday 11am - 6pm
   </p>
-  <div class="video-container">
+  <!--<div class="video-container">
     <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/R4O1LidlEr0" frameborder="0" allowfullscreen></iframe>
   </div>
-  <span style="font-size:0.9em;">Musique Plastique by Jay Winebrenner
+  <span style="font-size:0.9em;">Musique Plastique by Jay Winebrenner-->
 
 
 <?php
